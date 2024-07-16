e716b5129e2448a263bbcb73d0dcbc41fd6b898e
Velocity <velocityreact@vctcpune.com>
2024-07-16 08:13:53 +0530

    Added bootstrap and MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Paper } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-component mt-5'>
+       
+       <Paper elevation={3}>
+          APP component
+         </Paper>
     </div>
   );
 }
